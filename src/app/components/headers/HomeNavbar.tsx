1ef63f8dfc9cd57218256b5b274cb0d94dce2f8e
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-07-26 02:43:11 +0900

    feat: upload client materials

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,3 +1,14 @@
+import { Container, Stack } from "@mui/material";
+
 export function HomeNavbar() {
-  return <div>HomeNavbar</div>;
+  return (
+    <div className="home-navbar">
+      <Container sx={{ mt: "55px", height: "642px" }}>
+        <Stack>
+            
+        </Stack>
+        <Stack>Details</Stack>
+      </Container>
+    </div>
+  );
 }
