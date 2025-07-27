c570f67ea0d24187d4c95a649ab1968ddf77d0dd
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-07-27 20:37:29 +0900

    fix: modify homeNavbar header component

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -6,19 +6,13 @@ export function HomeNavbar() {
   return (
     <div className="home-navbar">
       <Container className="navbar-container">
-        <Stack
-         className="menu"
-        >
+        <Stack className="menu">
           <Box>
             <NavLink to={"/"}>
-              <img
-               className="brand-logo"
-              />
+              <img className="brand-logo" src="/icons/burak.svg" />
             </NavLink>
           </Box>
-          <Stack
-           className="links"
-          >
+          <Stack className="links">
             <Box className={"hover-line"}>
               <NavLink to="/" activeClassName="underline">
                 Home
@@ -56,23 +50,34 @@ export function HomeNavbar() {
 
             {!authMember ? (
               <Box>
-                <Button
-                  variant="contained"
-                 className="login-button"
-                >
+                <Button variant="contained" className="login-button">
                   Login
                 </Button>
               </Box>
             ) : (
               <img
-               className="user-avatar"
+                className="user-avatar"
                 src="/icons/default-user.svg"
                 aria-haspopup={"true"}
               />
             )}
           </Stack>
         </Stack>
-        <Stack>Details</Stack>
+        <Stack className="header-frame">
+          <Stack className="detail">
+            <Box className="head-main-txt">World's Most Delicious Cousine</Box>
+            <Box className="wel-txt">The Choice, not just a choice</Box>
+            <Box className="service-txt">24 hours service</Box>
+            <Box className="signup">
+              {!authMember ? (
+                <Button variant="contained" className="signup-button">SIGN UP</Button>
+              ) : null}
+            </Box>
+          </Stack>
+          <Stack className="logo-frame">
+            <div className="logo-img"></div>
+          </Stack>
+        </Stack>
       </Container>
     </div>
   );
