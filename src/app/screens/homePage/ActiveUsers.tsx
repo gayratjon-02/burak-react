b84edbe468fea66eac63c20b0ac70503cb40e04b
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-07-30 06:18:09 +0900

    feat: develop actUserPage standards

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import { CssVarsProvider } from "@mui/joy/styles";
+
+const activeUsers = [
+  { memberNick: "Martin", memberImage: "/img/martin.webp" },
+  { memberNick: "Justin", memberImage: "/img/justin.webp" },
+  { memberNick: "Rose", memberImage: "/img/rose.webp" },
+  { memberNick: "Nusret", memberImage: "/img/buster.webp" },
+];
 
 export default function ActiveUsers() {
   return (
-    <div>
-      <Container>ActiveUsers</Container>
+    <div className="active-users-frame">
+      <Container>
+        <Stack className="main">
+          <Box className="category-title">Popular Dishes</Box>
+          <Stack className="cards-frame">
+            {activeUsers.length !== 0 ? (
+              activeUsers.map((ele, index) => {
+                return <CssVarsProvider>
+                  
+                </CssVarsProvider>;
+              })
+            ) : (
+              <Box className="no-data">New products are not available</Box>
+            )}
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
