e709bce7a71ccaf34e62735264e98f3d96e21bd0
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-08-05 19:54:50 +0900

    feat: publish order-right &&  PausedOrders sectional component

--- src/app/screens/ordersPage/index.tsx
+++ src/app/screens/ordersPage/index.tsx
@@ -7,6 +7,8 @@ import { TabContext } from "@mui/lab";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import "../../../css/order.css";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 export default function OrdersPage() {
   // hooks
@@ -43,6 +45,52 @@ export default function OrdersPage() {
               <FinsihedOrders />
             </Stack>
           </TabContext>
+        </Stack>
+
+        <Stack className="order-right">
+          <Stack className="order-right-top">
+            <Stack className="order-img-box">
+              <img className="main-img" src="/img/justin.webp" width={"100"} />
+              <AccountCircleIcon className="account-icon" />
+              <span>Justin</span>
+              <h1>User</h1>
+            </Stack>
+            <Stack className="order-location-box">
+              <div className="location-line"></div>
+              <Stack className="location-box">
+                <LocationOnIcon className="location-icon" />
+                <p>South Korea, Busan</p>
+              </Stack>
+            </Stack>
+          </Stack>
+          <Stack className="order-right-bott">
+            <Stack className="card-detail-wrapper">
+              <input
+                type="number"
+                placeholder="Card number : 5243 4090 2002 7495"
+              />
+              <Stack className="card-info">
+                <input type="text" placeholder="MM / YY" />
+                <input type="number" placeholder="CVV : 010" />
+              </Stack>
+              <input type="text" name="" id="" placeholder="Justin Robertson" />
+            </Stack>
+
+            <Stack className="card-images">
+              <a href="#">
+                <img src="/icons/western-card.svg" alt="" />
+              </a>
+              <a href="#">
+                <img src="/icons/master-card.svg" alt="" />
+              </a>
+              <a href="#">
+                <img src="/icons/paypal-card.svg" alt="" />
+              </a>
+              <a href="#">
+                <img src="/icons/visa-card.svg" alt="" />
+              </a>
+            </Stack>
+          </Stack>
         </Stack>
       </Container>
     </div>
