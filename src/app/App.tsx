f3517c3b6f8e88e81a29a2525b5c94e7c13843af
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-08-22 19:38:29 +0900

    feat: create Test class component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,6 +14,7 @@ import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
 import "../css/products.css";
+import Test from "./screens/Test";
 
 function App() {
   const location = useLocation();
@@ -36,7 +37,7 @@ function App() {
           <HelpPage />
         </Route>
         <Route path="/">
-          <HomePage />
+          <Test />
         </Route>
       </Switch>
       <Footer />
