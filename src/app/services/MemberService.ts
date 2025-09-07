33f7bd588cc786deb8b055dd8be4b5e70c08383c
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-09-07 14:25:41 +0500

    feat: create useGlobals integrated with Context & develop logout process

--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -54,7 +54,6 @@ class MemberService {
     }
   }
 
-
   public async login(input: LoginInput): Promise<Member> {
     try {
       const url = this.path + "/member/login";
@@ -68,6 +67,20 @@ class MemberService {
       return member;
     } catch (err) {
       console.log("Error, login:", err);
+      throw err;
+    }
+  }
+
+  public async logout(): Promise<void> {
+    try {
+      const url = this.path + "/member/logout";
+      const result = await axios.post(url, {}, { withCredentials: true });
+      console.log("logout", result);
+      localStorage.removeItem("memberData");
+
+      return result.data.logout;
+    } catch (err) {
+      console.log("Error, logout:", err);
       throw err;
     }
   }
